c9561081d4cd210bba1290e0049eceb0c7ab6326
Hikmahx <hikmayousuph@gmail.com>
2020-04-11 21:05:14 -0700

    created new folders template and posts

--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def index(request):
+    #return HttpResponse("HELLO WORLD FROM POSTS")
+    #for rendering template
+    return render(request, 'posts/index.html')
